b6f9636e40265cb757d000c4b4b60b92954a4263
Maulik Ranadive <80736633+Maulik176@users.noreply.github.com>
2022-04-02 10:24:59 +0530

    Time: 73 ms (97.99%), Space: 15.3 MB (10.49%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def validPalindrome(self, s):
+        for i in range(len(s) // 2):
+            if s[i] != s[~i]:
+                l = s[i:~i]
+                r = s[i+1:len(s)-i]
+                return l == l[::-1] or r == r[::-1]
+        return True
+        
