226ac76711acdda2d0e210f4c22effa590290e81
Maulik Ranadive <80736633+Maulik176@users.noreply.github.com>
2022-04-05 10:32:55 +0530

    Time: 819 ms (34.42%), Space: 24 MB (23.14%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -4,7 +4,7 @@ class Solution(object):
         j = len(height)-1  #last index in array
         water_amount = 0 
         while i<j:
-            store = max(water_amount,  (j-i)*min(height[i], height[j]))
+            store = max(water_amount,  (j-i)*min(height[i],height[j]))
             water_amount = max(water_amount,store)
             if(height[i] < height[j]):
                 i +=1
