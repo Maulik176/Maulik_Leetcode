fcfd44f26647d2690f3c79a8b67134f52d5d5311
Maulik Ranadive <80736633+Maulik176@users.noreply.github.com>
2022-04-09 11:00:41 +0530

    Time: 157 ms (37.98%), Space: 18.6 MB (68.16%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,6 +1,6 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        temp = Counter(nums).most_common(k)
+        temp = Counter(nums).most_common(k) #counter keeps a count of the most frequent number .... counter(nums).most_common(k) keeps a counter od the most common element asked for(k)
         ans =[]
         for i in range(k):
             ans.append(temp[i][0])
