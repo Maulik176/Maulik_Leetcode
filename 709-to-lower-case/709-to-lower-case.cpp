5ff70d359fdeb801e71c961e4bb43fd912910fff
Maulik Ranadive <80736633+Maulik176@users.noreply.github.com>
2022-06-23 09:40:28 +0530

    Time: 6 ms (10.82%), Space: 6.2 MB (51.99%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string toLowerCase(string s) 
+    {
+        for(int i=0;i< size(s);i++)
+        {
+            if(isupper(s[i]))
+            {
+                s[i]=tolower(s[i]);
+            }
+        }
+        return s;
+    }
+};
