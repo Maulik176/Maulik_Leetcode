c90d2d8457465580e26902d097d3099369d932ed
Maulik Ranadive <80736633+Maulik176@users.noreply.github.com>
2022-08-01 13:33:20 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>> &dp, int& m, int& n) {
-        if(i == m-1 /*last row*/ || j == n-1 /*last column*/) return 1;
+    int solve(int i, int j, vector<vector<int>>& dp, int m, int n){
+        if(i == m-1 || j == n-1){ 
+            //if the robot is located at last row or column
+            return 1;
+        }
         if(dp[i][j] != 0){
             return dp[i][j];
         }
-        
-        dp[i][j] = solve(i+1, j, dp, m, n) + solve(i, j+1, dp, m, n);
+        dp[i][j] += solve(i+1 , j , dp, m,n) + solve(i, j+1 , dp,m,n);
         return dp[i][j];
-    }
+    }  
+    
     
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(m , vector<int>(n , 0));
         return solve(0, 0, dp, m, n);
     }
 };
+
