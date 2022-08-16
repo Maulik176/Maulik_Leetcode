4cfaef48e16fc253bd4662e4c4bd429a83d48cbb
Maulik Ranadive <80736633+Maulik176@users.noreply.github.com>
2022-08-16 11:40:19 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (85.13%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        map<char,int> mp;
+        for(auto i:s){
+            mp[i]++;
+            if(mp[i] == 2)
+                return i;
+        }
+        return ' ';
+    }
+};
