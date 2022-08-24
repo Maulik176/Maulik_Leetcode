b0053748d238d0d40315850dfe195bd38fa259d7
Maulik Ranadive <80736633+Maulik176@users.noreply.github.com>
2022-08-24 11:00:46 +0530

    Time: 16 ms (94.46%), Space: 5.7 MB (98.87%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -3,7 +3,7 @@ public:
     bool isPowerOfThree(int n) {
         if(n<=0) return false;
         while(n>1){
-            if (n%3 != 0) return false;
+            if(n%3 != 0) return false;
             n = n/3;
         }
         return true;
