acb698db90433a399deaadf38c6e5b21aa7e905e
Maulik Ranadive <80736633+Maulik176@users.noreply.github.com>
2022-08-25 11:41:10 +0530

    Time: 26 ms (52.04%), Space: 8.9 MB (15.41%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -3,7 +3,7 @@ public:
     bool canConstruct(string a, string b) {
         int size_a = a.size();
         int size_b = b.size();
-        // if(a>b) return false;
+        if(size_a>size_b) return false;
         unordered_map<char, int> ransom;
         unordered_map<char, int> magazine;
         for(auto it:a) ransom[it]++;
