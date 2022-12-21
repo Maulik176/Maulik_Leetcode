f05efc3e65a60a276b25714bf332cf11d5873f00
Maulik Ranadive <80736633+Maulik176@users.noreply.github.com>
2022-12-21 13:07:13 +0530

    Added solution - LeetHub

---
+++ Search in Rotated Array 2 - GFG/search-in-rotated-array-2.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool Search(int N, vector<int>& A, int Key) {
+        int start = A[0];
+        int end = A[N];
+        for(int i=0;i<N;i++){
+            if(A[i]==Key) return 1;
+        }// Code here
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a;
+
+        for (int i = 0; i < n; ++i) {
+            int x;
+            cin >> x;
+            a.push_back(x);
+        }
+        int key;
+        cin >> key;
+
+        Solution obj;
+        cout << obj.Search(n, a, key) << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
